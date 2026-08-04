c01eef0758cadbcc09b406676cba7ccd499d3e6f
bengsfort <bengsfort.dev@gmail.com>
2026-08-04 14:56:10 +0300

    Add fix_zsh_history function for fixing corrupt history files

---
+++ zsh/functions.zsh
@@ -0,0 +1,10 @@
+# zsh functions
+
+function fix_zsh_history() {
+	mv ~/.zsh_history ~/.zsh_history_bad;
+	strings ~/.zsh_history_bad > ~/.zsh_history;
+	fc -R ~/.zsh_history;
+	rm ~/.zsh_history_bad;
+}
+
+echo "zsh functions completed"
